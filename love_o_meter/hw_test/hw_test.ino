bfbaadae42b80c6b70027788223bc582db3866d6
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-11 23:27:13 +0100

    Simple hardware test unit

--- love_o_meter/hw_test/hw_test.ino
+++ love_o_meter/hw_test/hw_test.ino
@@ -4,6 +4,8 @@ const int ledOne = 2;
 const int ledTwo = 3;
 const int ledThree = 4;
 
+float temperature = 25.0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(ledOne, OUTPUT);
@@ -12,9 +14,29 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ledOne, HIGH);
-  digitalWrite(ledTwo, HIGH);
-  digitalWrite(ledThree, HIGH);
+  if (temperature <= 22.0) {
+    digitalWrite(ledOne, LOW);
+    digitalWrite(ledTwo, LOW);
+    digitalWrite(ledThree, LOW);  
+  } 
+  
+  if (temperature > 22.0) {
+    digitalWrite(ledOne, HIGH);
+    digitalWrite(ledTwo, LOW);
+    digitalWrite(ledThree, LOW);  
+  }
+  
+  if (temperature > 24.0) {
+    digitalWrite(ledOne, HIGH);
+    digitalWrite(ledTwo, HIGH);
+    digitalWrite(ledThree, LOW);  
+  }
+  
+  if (temperature > 26.0) {
+    digitalWrite(ledOne, HIGH);
+    digitalWrite(ledTwo, HIGH);
+    digitalWrite(ledThree, HIGH);  
+  }
   
   delay(1);
 }
