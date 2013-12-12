e3c4035d99ec284be2b1ca78d6fa45e55a669e2d
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-12 01:40:49 +0100

    Switch button for diagnostics mode. Full data printing from sensor.

--- love_o_meter/project/project.ino
+++ love_o_meter/project/project.ino
@@ -4,6 +4,7 @@ const int sensorPin = A0;
 const int ledOne = 2;
 const int ledTwo = 3;
 const int ledThree = 4;
+const int switchButton = 5;
 
 const float baselineTemp = 22.0;
 
@@ -12,16 +13,26 @@ void setup() {
   pinMode(ledOne, OUTPUT);
   pinMode(ledTwo, OUTPUT);
   pinMode(ledThree, OUTPUT);
+  pinMode(switchButton, INPUT);
 }
 
 void loop() {
+  int deviceStatus = digitalRead(switchButton);
+
   int sensorVal = analogRead(sensorPin);
   float voltage = (sensorVal/1024.0) * 5.0;
   float temperature = (voltage - .5) * 100;
 
-  loveMeterLed(temperature);
+  if (deviceStatus == 0) {
+    loveMeterLed(temperature);
+  } else {
+    printTemperature(sensorVal, voltage, temperature);
+    debugLed(20.0);
+    debugLed(23.0);
+    debugLed(25.0);
+    debugLed(28.0);
+  }
 
-  printTemperature(temperature);
   delay(1);
 }
 
@@ -51,7 +62,17 @@ void loveMeterLed(float temperature) {
   }
 }
 
-void printTemperature(float sensorVal) {
-  Serial.print("Temperature: ");
-  Serial.println(sensorVal);
+void printTemperature(int sensorVal, float voltage, float temperature) {
+  Serial.print("Sensor value: ");
+  Serial.print(sensorVal);
+  Serial.print(", Voltage: ");
+  Serial.print(voltage);
+  Serial.print(", Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+}
+
+void debugLed(float temperature) {
+  loveMeterLed(temperature);
+  delay(50);
 }
