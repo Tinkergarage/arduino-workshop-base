de283aa2a60453bbdba9c93004b8edb19065aa49
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-19 10:50:34 +0100

    Motorized Pinwheel project

---
+++ motorized_pinwheel/project/project/project.ino
@@ -0,0 +1,20 @@
+ /* Motorized Pinwheel */
+ 
+const int switchPin = 2;
+const int motorPin = 9;
+int switchState = LOW;
+
+void setup() {
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(switchPin);
+  
+  if (switchState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  } else {
+    digitalWrite(motorPin, LOW);
+  }
+}
